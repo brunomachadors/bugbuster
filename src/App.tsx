093f35b62aebbe0d13435a4fc2587a2411a2ed22
brunomachadors <brunomachadors@gmail.com>
2024-06-02 22:21:54 +0100

    Fix refresh page

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom';
-import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Mentors from './pages/Mentors';
-import NotFound from './pages/NotFound'; // Importe o componente NotFound
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    localStorage.setItem('lastPage', location.pathname);
-  }, [location]);
-
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mentors" element={<Mentors />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </>
-  );
-}
-
-function AppWrapper() {
   return (
     <Router basename="/bugbuster">
-      <App />
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/mentors" element={<Mentors></Mentors>} />
+      </Routes>
+      <Footer></Footer>
     </Router>
   );
 }
 
-export default AppWrapper;
+export default App;
